3ba98f2e446d39a2bf264f359b0c66349bdd1997
aml walaed <109831373+amlWalaed@users.noreply.github.com>
2024-10-06 15:58:24 +0300

    [feature]:add layout

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+const head = useLocaleHead({
+  addDirAttribute: true,
+  identifierAttribute: "id",
+  addSeoAttributes: true,
+})
+</script>
+
 <template>
+  <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir"></Html>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLoadingIndicator color="hsl(var(--primary) / 1)" :height="3" />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { RouteLocationRaw } from "vue-router"
+
+const localePath = useLocalePath()
+
+const navLinks = computed<
+  {
+    label: string
+    to: RouteLocationRaw
+  }[]
+>(() => {
+  return [
+    {
+      label: "Home",
+      to: { name: "index" },
+    },
+    {
+      label: "Promo",
+      to: { name: "" },
+    },
+    {
+      label: "My Booking",
+      to: { name: "" },
+    },
+  ]
+})
+</script>
+<template>
+  <div>
+    <nav class="container flex items-center justify-between gap-3 py-3">
+      <AppLogo />
+      <ul class="flex items-center gap-3">
+        <template v-for="navLink in navLinks" :key="link.label">
+          <li>
+            <NuxtLink :to="localePath(navLink.to)">{{
+              navLink.label
+            }}</NuxtLink>
+          </li>
+        </template>
+      </ul>
+      <Button>Sign in </Button>
+    </nav>
+    <main>
+      <NuxtPage />
+    </main>
+  </div>
+</template>
