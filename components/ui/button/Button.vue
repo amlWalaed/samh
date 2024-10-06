c8528346bcab2e87a3eeb03fc4a01cea01548cc3
aml walaed <109831373+amlWalaed@users.noreply.github.com>
2024-10-06 20:11:08 +0300

    enhance button

--- components/ui/button/Button.vue
+++ components/ui/button/Button.vue
@@ -1,26 +1,153 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { Primitive, type PrimitiveProps } from 'radix-vue'
-import { type ButtonVariants, buttonVariants } from '.'
-import { cn } from '@/utils'
+import { cn } from "@/utils"
 
-interface Props extends PrimitiveProps {
-  variant?: ButtonVariants['variant']
-  size?: ButtonVariants['size']
-  class?: HTMLAttributes['class']
-}
+import { Primitive } from "radix-vue"
+import { buttonVariants, buttonIconVariants, type ButtonProps } from "."
 
-const props = withDefaults(defineProps<Props>(), {
-  as: 'button',
+import type { NuxtLinkProps } from "#app"
+import { NuxtLink } from "#components"
+
+const props = withDefaults(
+  defineProps<Partial<ButtonProps & { to?: NuxtLinkProps["to"] }>>(),
+  {
+    as: "button",
+    type: "button",
+    loadingIcon: "i-app-loading",
+  }
+)
+
+const slots = useSlots()
+const localePath = useLocalePath()
+
+const isString = (value: any): value is string => typeof value === "string"
+const isDisabled = computed(() => props.disabled || props.loading)
+const isLink = computed(
+  () => (isString(props.as) && props.as?.toLowerCase() === "link") || props.to
+)
+
+const LinkWithAsProps = computed(() =>
+  h(
+    NuxtLink,
+
+    { as: props.linkAs as string, to: localePath(props.to) } as NuxtLinkProps,
+
+    {
+      default: slots?.default,
+    }
+  )
+)
+
+const resolvedAs = computed(() =>
+  isLink.value && props.as !== "a" ? LinkWithAsProps.value : props.as
+)
+
+const isLeading = computed(() => {
+  return (
+    (props.icon && props.leading) ||
+    (props.icon && !props.trailing) ||
+    props.leadingIcon
+  )
 })
+
+const isTrailing = computed(() => {
+  return (props.icon && props.trailing) || props.trailingIcon
+})
+const isTrailingOnly = computed(() => isTrailing.value && !isLeading.value)
+const hasIcon = computed(() => {
+  return isTrailing.value || isLeading.value
+})
+
+const iconOnly = computed(() => {
+  return hasIcon.value && !props.label?.length && !slots?.default
+})
+
+const leadingIconName = computed(() => {
+  return props.loading ? props.loadingIcon : props.leadingIcon || props.icon
+})
+
+const trailingIconName = computed(() => {
+  return props.loading && isTrailingOnly.value
+    ? props.loadingIcon
+    : props.trailingIcon || props.icon
+})
+
+const leadingIconClass = computed(() => {
+  return buttonIconVariants({ size: props.size })
+})
+
+const trailingIconClass = computed(() => {
+  return buttonIconVariants({ size: props.size })
+})
+const buttonId = props.id ?? `${useId()}-button`
 </script>
 
 <template>
   <Primitive
-    :as="as"
-    :as-child="asChild"
-    :class="cn(buttonVariants({ variant, size }), props.class)"
+    :aria-disabled="isDisabled"
+    :aria-busy="loading"
+    :disabled="isDisabled"
+    :type="type"
+    :as="resolvedAs"
+    :as-child="props.asChild"
+    :id="buttonId"
+    :class="
+      cn(
+        buttonVariants({ variant, size, color, roundness }),
+        block && 'flex w-full',
+        disabled && 'btn-disabled',
+        props.class
+      )
+    "
   >
-    <slot />
+    <div
+      v-if="props.loading && !hasIcon"
+      class="absolute inset-0 grid place-items-center"
+    >
+      <span class="i-app-loading-dots h-3 w-1/2 max-w-[90%]" />
+    </div>
+    <span
+      v-if="(isLeading && leadingIconName) || $slots.leading"
+      :class="cn('inline-flex items-center', leadingIconWrapperClass)"
+    >
+      <slot name="leading" :disabled="props.disabled" :loading="loading">
+        <span
+          :class="cn(leadingIconClass, leadingIconName)"
+          aria-hidden="true"
+        />
+      </slot>
+    </span>
+    <span
+      v-if="label || $slots.default"
+      :class="
+        cn('inline-flex items-center', [
+          props.loading && !hasIcon && 'invisible',
+          props.ui?.label,
+        ])
+      "
+    >
+      <slot>
+        <span
+          v-if="label"
+          :class="
+            cn('first-letter:capitalize', {
+              'line-clamp-1 break-all text-left': truncate,
+            })
+          "
+        >
+          {{ label }}
+        </span>
+      </slot>
+    </span>
+    <span
+      v-if="(isTrailing && trailingIconName) || $slots.trailing"
+      :class="cn('inline-flex items-center', trailingIconWrapperClass)"
+    >
+      <slot name="trailing" :disabled="props.disabled" :loading="loading">
+        <span
+          :class="cn(trailingIconClass, trailingIconName)"
+          aria-hidden="true"
+        />
+      </slot>
+    </span>
   </Primitive>
 </template>
--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -1,4 +1,7 @@
 import { type VariantProps, cva } from "class-variance-authority"
+import type { ClassValue } from "clsx"
+import type { PrimitiveProps } from "radix-vue"
+import type { HTMLAttributes } from "vue"
 
 export { default as Button } from "./Button.vue"
 
@@ -19,17 +22,65 @@ export const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
-        xs: "h-7 rounded px-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        xs: "h-7 px-2",
+        sm: "h-9 px-3",
+        lg: "h-11 px-8",
         icon: "h-10 w-10",
+      },
+      roundness: {
+        none: "rounded-none",
+        sm: "rounded-sm",
+        md: "rounded-md",
+        lg: "rounded-lg",
       },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      roundness: "md",
     },
   }
 )
 
 export type ButtonVariants = VariantProps<typeof buttonVariants>
+export const buttonIconVariants = cva("shrink-0", {
+  variants: {
+    size: {
+      default: "size-5",
+      sm: "size-3",
+      lg: "size-6",
+      icon: "size-5",
+      "icon-sm": "size-4",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+})
+
+export interface ButtonProps {
+  as: PrimitiveProps["as"] | Component | "link"
+  asChild: PrimitiveProps["asChild"]
+  variant?: ButtonVariants["variant"]
+  size?: ButtonVariants["size"]
+  roundness?: ButtonVariants["roundness"]
+  class?: HTMLAttributes["class"]
+  type: "button" | "submit" | "reset"
+  linkAs: PrimitiveProps["as"] | Component
+  loading: boolean
+  disabled: boolean
+  label: string
+  truncate: boolean
+  block: boolean
+  loadingIcon: string
+  icon: ClassValue
+  trailing: boolean
+  leading: boolean
+  leadingIcon: ClassValue
+  leadingIconWrapperClass: ClassValue
+  trailingIcon: ClassValue
+  trailingIconWrapperClass: ClassValue
+  ui: {
+    label: ClassValue
+  }
+}
