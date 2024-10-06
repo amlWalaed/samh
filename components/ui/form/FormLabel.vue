80558f396c31aff28899b8acba5f2f6a2cab78cc
aml walaed <109831373+amlWalaed@users.noreply.github.com>
2024-10-06 20:12:06 +0300

    add icon beside label

--- components/ui/form/FormLabel.vue
+++ components/ui/form/FormLabel.vue
@@ -1,23 +1,34 @@
 <script lang="ts" setup>
-import type { HTMLAttributes } from 'vue'
-import type { LabelProps } from 'radix-vue'
-import { useFormField } from './useFormField'
-import { cn } from '@/utils'
-import { Label } from '@/components/ui/label'
+import type { HTMLAttributes } from "vue"
+import type { LabelProps } from "radix-vue"
+import { useFormField } from "./useFormField"
+import { cn } from "@/utils"
+import { Label } from "@/components/ui/label"
+import type { ClassValue } from "clsx"
 
-const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<
+  LabelProps & {
+    class?: HTMLAttributes["class"]
+    icon?: ClassValue
+    value?: string
+  }
+>()
 
 const { error, formItemId } = useFormField()
 </script>
 
 <template>
   <Label
-    :class="cn(
-      error && 'text-destructive',
-      props.class,
-    )"
+    :class="
+      cn(['flex gap-2 items-center', error && 'text-destructive', props.class])
+    "
     :for="formItemId"
   >
-    <slot />
+    <span v-if="icon" :class="cn(['size-5 text-primary', icon])" />
+    <slot>
+      <span class="first-letter:capitalize" v-if="value || $slots.default">
+        {{ value }}
+      </span>
+    </slot>
   </Label>
 </template>
