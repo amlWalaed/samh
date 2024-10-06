8676151064d438d73bfd9d339ddcd0bdffb62e8a
aml walaed <109831373+amlWalaed@users.noreply.github.com>
2024-10-06 17:05:12 +0300

    [feature] add favicon and fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,53 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon-32x32.png",
+          sizes: "32x32",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon-16x16.png",
+          sizes: "16x16",
+        },
+        { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap",
+        },
+      ],
+      meta: [
+        {
+          name: "theme-color",
+          content: "#EAA121",
+        },
+      ],
+      titleTemplate: "%s | GSE",
+    },
+  },
 
   i18n: {
     lazy: true,
--- tailwind.config.js
+++ tailwind.config.js
@@ -100,6 +100,10 @@ module.exports = {
           to: { height: 0 },
         },
       },
+      fontFamily: {
+        "playfair-display": ["Playfair Display", "system-ui"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
